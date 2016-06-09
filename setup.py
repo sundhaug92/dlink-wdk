6cda8b00d82be389b9d6bf93d62505b0edbfd126
Martin <martinsundhaug@gmail.com>
2016-06-09 17:25:00 +0200

    Updated metadata files

--- setup.py
+++ setup.py
@@ -3,10 +3,26 @@
 from distutils.core import setup
 
 setup(name='WDK',
-      version='1.0',
+      version='1.0.1b1',
       description='Python API for D-Link WDK',
       author='Martin Sundhaug',
       author_email='martinsundhaug@gmail.com',
       url='https://github.com/sundhaug92/dlink-wdk',
-      py_modules=['wdk']
+      py_modules=['wdk'],
+      license='MIT',
+      classifiers=[
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: Only',
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Topic :: Security',
+          'Topic :: Software Development :: Embedded Systems',
+          'Topic :: Software Development :: Libraries'
+      ],
+      keywords='dlink dir100 wdk web cliget api'
 )
